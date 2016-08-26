6c16df7689aaa57295bb51d9440ee6d13ed6bab9
Piotr Żórawski <piotrzorawski@gmail.com>
2016-08-26 15:03:44 +0200

    Add files via upload

--- movementengine.js
+++ movementengine.js
@@ -27,45 +27,45 @@ function move(y,x){
 function makeMoveButtonsActive(){
 	if (currentRoom.up === true){
 	document.getElementById("Button1").onclick = function(){move(1, 0);}; //UP
-	document.getElementById("Button1").className = "fadeIn"; //FadeIn +
+	document.getElementById("Button1").className += " fadeIn"; //FadeIn +
 	document.getElementById("Button1").className.replace(/\bfadeOut\b/,''); // FadeOut -
 	document.getElementById("Button1").disabled = false;
 	} else {
 	document.getElementById("Button1").onclick = "";
-	document.getElementById("Button1").className += "fadeOut"; // FadeOut +
+	document.getElementById("Button1").className += " fadeOut"; // FadeOut +
 	document.getElementById("Button1").className.replace(/\bfadeIn\b/,''); // FadeIn -
 	document.getElementById("Button1").disabled = true;
 	}
 	if (currentRoom.down === true){
 	document.getElementById("Button4").onclick = function(){move(-1, 0);}; //DOWN
-	document.getElementById("Button4").className = "fadeIn"; //FadeIn +
+	document.getElementById("Button4").className += " fadeIn"; //FadeIn +
 	document.getElementById("Button4").className.replace(/\bfadeOut\b/,''); // FadeOut -
 	document.getElementById("Button4").disabled = false;
 	} else {
 	document.getElementById("Button4").onclick = "";
-	document.getElementById("Button4").className += "fadeOut"; // FadeOut +
+	document.getElementById("Button4").className += " fadeOut"; // FadeOut +
 	document.getElementById("Button4").className.replace(/\bfadeIn\b/,''); // FadeIn -
 	document.getElementById("Button4").disabled = true;
 	}
 	if (currentRoom.left === true){
 	document.getElementById("Button2").onclick = function(){move(0, -1);}; //LEFT
-	document.getElementById("Button2").className = "fadeIn"; //FadeIn +
+	document.getElementById("Button2").className += " fadeIn"; //FadeIn +
 	document.getElementById("Button2").className.replace(/\bfadeOut\b/,''); // FadeOut -
 	document.getElementById("Button2").disabled = false;
 	} else {
 	document.getElementById("Button2").onclick = "";
-	document.getElementById("Button2").className += "fadeOut"; // FadeOut +
+	document.getElementById("Button2").className += " fadeOut"; // FadeOut +
 	document.getElementById("Button2").className.replace(/\bfadeIn\b/,''); // FadeIn -
 	document.getElementById("Button2").disabled = true;
 	}
 	if (currentRoom.right === true){
 	document.getElementById("Button3").onclick = function(){move(0, 1);}; //RIGHT
-	document.getElementById("Button3").className = "fadeIn"; //FadeIn +
+	document.getElementById("Button3").className += " fadeIn"; //FadeIn +
 	document.getElementById("Button3").className.replace(/\bfadeOut\b/,''); // FadeOut -
 	document.getElementById("Button3").disabled = false;
 	} else {
 	document.getElementById("Button3").onclick = "";
-	document.getElementById("Button3").className += "fadeOut"; // FadeOut +
+	document.getElementById("Button3").className += " fadeOut"; // FadeOut +
 	document.getElementById("Button3").className.replace(/\bfadeIn\b/,''); // FadeIn -
 	document.getElementById("Button3").disabled = true;
 	}
